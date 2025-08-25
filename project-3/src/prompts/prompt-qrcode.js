5f1a47eecf79c554bcc229028075ab4a76043e95
Luis Guedes Dev <luisgcostaguedes@gmail.com>
2025-08-25 10:56:31 -0300

    up qr code generate

--- project-3/src/prompts/prompt-qrcode.js
+++ project-3/src/prompts/prompt-qrcode.js
@@ -1,7 +1,19 @@
 import chalk from "chalk";
 
+const promptQRCode = [
+  {
+    name: "link",
+    description: chalk.yellow("Digite o link para gerar o QR CODE"),
+  },
+  {
+    name: "type",
+    description: chalk.yellow(
+      "Escolha o tipo onde será apresentado o QR CODE (1 - Normal ou 2 - Terminal))"
+    ),
+    pattern: /^[1-2]+$/,
+    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    require: true,
+  },
+];
 
-const promptQRCode = [];
-
-
-export default promptQRCode()
+export default promptQRCode;
--- project-3/src/services/qr-code/create.js
+++ project-3/src/services/qr-code/create.js
@@ -1,5 +1,9 @@
 import prompt from "prompt";
-import promptQrcode from "../../prompts/prompt-qrcode";
-async function createQRCode() {}
+import promptQRcode from "../../prompts/prompt-qrcode.js";
+import handle from "./handle.js";
+async function createQRCode() {
+  prompt.get(promptQRcode, handle);
+  prompt.start();
+}
 
 export default createQRCode();
