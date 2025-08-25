4de1d432045200466a811e60e827c81a6e466361
Luis Guedes Dev <luisgcostaguedes@gmail.com>
2025-08-25 17:00:38 -0300

    add err

--- project-3/src/index.js
+++ project-3/src/index.js
@@ -5,6 +5,10 @@ import createPassword from "./services/password/create.js";
 
 async function main() {
   prompt.get(promptSchemaMain, async (err, choose) => {
+    if (err) {
+      console.log(err);
+    }
+
     if (choose.select == 1) await createQRCode();
     if (choose.select == 2) await createPassword();
   });
