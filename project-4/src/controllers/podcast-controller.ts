24dc384229c9e00c0b7814f5c39625035498228d
Luis Guedes Dev <luisgcostaguedes@gmail.com>
2025-09-11 11:22:47 -0300

    finish routes

--- project-4/src/controllers/podcast-controller.ts
+++ project-4/src/controllers/podcast-controller.ts
@@ -1,11 +1,21 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { serviceListEpisodes } from "../services/list-episodes-service";
+import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 
 export const getListEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
   const content = await serviceListEpisodes();
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(content));
+};
+
+export const getFilterEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  const content = await serviceFilterEpisodes("Flow");
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify(content));
 };
--- project-4/src/services/filter-episodes-service.ts
+++ project-4/src/services/filter-episodes-service.ts
@@ -2,4 +2,5 @@ import { repositoryPodcast } from "../repositories/podcasts-repository";
 
 export const serviceFilterEpisodes = async (podcastName: string) => {
   const data = await repositoryPodcast(podcastName);
+  return data;
 };
